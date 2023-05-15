093d4641ea4f8f94c638b039a0720998313e0b1a
Tiago Nelson Pinheiro <55251934+Tiagonp@users.noreply.github.com>
2023-05-15 10:22:44 -0300

    Update vetor.cpp

--- vetor.cpp
+++ vetor.cpp
@@ -22,5 +22,25 @@ namespace prg2 {
     v.
   }
   
+  int &vetor_frente(VetorDinamico &v) {
+    if (v.len == 0) {
+      throw std::invalid_argument("Vetor vazio");
+    }
+    return v.mem[0];
+  }
+  
+  int &vetor_atras(VetorDinamico &v) {
+    if (v.len == 0) {
+      throw std::invalid_argument("vetor vazio");
+    }
+    return v.mem[v.len-1];
+  }
+  
+  int &vetor_obtem (VetorDinamico &v, int pos) {
+  
+    
+    return v.mem[pos];
+  }
+  
   
 }
